af4a3b669bd316ca6e6a9ea836f0077b2766c692
edureka <support.edureka.co>
2024-07-25 12:56:58 +0000

    My First commit

---
+++ java-project/Hello.java
@@ -0,0 +1,17 @@
+// File: HelloWorld.java
+
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Print "Hello, World!" to the console
+
+        System.out.println("Hello, World!");
+
+    }
+
+}
+
+
